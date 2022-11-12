374eee3c3bb32ddd80e71fdaf6cb5903a135a595
vargasmelina2001 <melina.vargas@est.ikiam.edu.ec>
2022-11-12 18:57:25 -0500

    Control de versión y clase4_until.sh
    Realizado por Melina Vargas

---
+++ Clase4/clase4_until.sh
@@ -0,0 +1,8 @@
+## Ejercicio 1.10.3 Plant-Pollinator Networks ##
+# 1. Escriba un script que tome uno de estos archivos y determine el número de filas (polinizadores) y el número de columnas (plantas). #
+cd CSB-master/unix/data/Saavedra2013
+ls -v
+
+bash ../scriptconcatenado.sh n5.txt
+
+
